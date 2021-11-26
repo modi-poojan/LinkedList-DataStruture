e29567f05280e70c23afad929c25d9f0da4f1631
modi-poojan <80687912+modi-poojan@users.noreply.github.com>
2021-11-26 22:53:14 +0530

    UC2: Linked List by adding 30 and 56 to 70

---
+++ src/com/LinkedList/Main_LinkedList.java
@@ -0,0 +1,14 @@
+package com.LinkedList;
+
+public class Main_LinkedList {
+
+	public static void main(String[] args) {
+		
+		LinkedList call = new LinkedList();
+		call.insertAtHead(70);
+		call.insertAtHead(30);
+		call.insertAtHead(56);
+		
+		call.display();
+	}
+}
