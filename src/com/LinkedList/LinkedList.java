e0be066fdc6f45be26d8572768f89124a0af410b
modi-poojan <80687912+modi-poojan@users.noreply.github.com>
2021-11-26 20:41:17 +0530

    UC1: Created a LinkedList for data 56-30-70

---
+++ src/com/LinkedList/LinkedList.java
@@ -0,0 +1,17 @@
+package com.LinkedList;
+
+public class LinkedList {
+	public static void main(String[] args) {
+		
+		Node node1 = new Node(56); 
+		Node node2 = new Node(30);
+		Node node3 = new Node(70); 
+		
+		node1.next = node2;
+		node2.next = node3;
+	}
+	
+	
+	
+	}
+
