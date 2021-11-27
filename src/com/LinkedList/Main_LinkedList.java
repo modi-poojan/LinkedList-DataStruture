db757d0fa65c8579b5f941afd069cde09fe4576f
modi-poojan <80687912+modi-poojan@users.noreply.github.com>
2021-11-27 07:51:25 +0530

    UC3: Ability to append LinkedList

--- src/com/LinkedList/Main_LinkedList.java
+++ src/com/LinkedList/Main_LinkedList.java
@@ -5,9 +5,9 @@ public class Main_LinkedList {
 	public static void main(String[] args) {
 		
 		LinkedList call = new LinkedList();
-		call.insertAtHead(70);
-		call.insertAtHead(30);
 		call.insertAtHead(56);
+		call.addToList(30);
+		call.addToList(70);
 		
 		call.display();
 	}
